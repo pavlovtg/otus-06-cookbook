786f76b26a5ba7e4c7c458d6b1a5f312c5dfb72d
tpavlov <tpavlov@example.com>
2026-06-13 14:58:23 +0300

    fix(domain): add JsonStringEnumMemberName snake_case attributes to IngredientCategory

--- apps/Cookbook/src/Recipes/Domain/IngredientCategory.cs
+++ apps/Cookbook/src/Recipes/Domain/IngredientCategory.cs
@@ -1,18 +1,45 @@
+using System.Text.Json.Serialization;
+
 namespace Recipes.Domain;
 
 internal enum IngredientCategory
 {
+    [JsonStringEnumMemberName("vegetables")]
     Vegetables,
+
+    [JsonStringEnumMemberName("fruits_and_berries")]
     FruitsAndBerries,
+
+    [JsonStringEnumMemberName("meat_and_poultry")]
     MeatAndPoultry,
+
+    [JsonStringEnumMemberName("fish_and_seafood")]
     FishAndSeafood,
+
+    [JsonStringEnumMemberName("dairy_and_eggs")]
     DairyAndEggs,
+
+    [JsonStringEnumMemberName("grains_and_cereals")]
     GrainsAndCereals,
+
+    [JsonStringEnumMemberName("legumes")]
     Legumes,
+
+    [JsonStringEnumMemberName("nuts_and_seeds")]
     NutsAndSeeds,
+
+    [JsonStringEnumMemberName("oils_and_fats")]
     OilsAndFats,
+
+    [JsonStringEnumMemberName("spices_and_seasonings")]
     SpicesAndSeasonings,
+
+    [JsonStringEnumMemberName("sauces_and_pastes")]
     SaucesAndPastes,
+
+    [JsonStringEnumMemberName("bakery_and_sweets")]
     BakeryAndSweets,
+
+    [JsonStringEnumMemberName("other")]
     Other,
 }
